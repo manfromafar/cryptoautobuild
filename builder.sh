60a300125b03e1e6008ab0d28b5c7ade249b7311
Mheetu <8495484+Mheetu@users.noreply.github.com>
2017-12-26 06:59:27 -0500

    Simplify directory creation

--- builder.sh
+++ builder.sh
@@ -23,19 +23,14 @@ displayErr() {
     exit 1;
 }
 
-cd ~
-if [[ ! -e 'CoinBuilds' ]]; then
-    sudo mkdir CoinBuilds
-elif [[ ! -d 'CoinBuilds' ]]; then
-    output "Coinbuilds already exists.... Skipping" 1>&2
-fi
-clear
+sudo mkdir -p ~/CoinBuilds
+cd ~/CoinBuilds
 
-cd CoinBuilds
 output "This script assumes you already have the dependicies installed on your system!"
 output ""
 read -e -p "Enter the name of the coin : " coin
 read -e -p "Paste the github link for the coin : " git_hub
+
 if [[ ! -e '$coin' ]]; then
     sudo  git clone $git_hub  $coin
 elif [[ ! -d ~$CoinBuilds/$coin ]]; then
@@ -51,13 +46,10 @@ if [ -f autogen.sh ]; then
     sudo make
     output "$coin_name finished and can be found in CoinBuilds/$coin/src/ Make sure you sudo strip Coind and coin-cli if it exists, copy to /usr/bin"
     output "Like my scripts? Please Donate to BTC Donation: 16xpWzWP2ZaBQWQCDAaseMZBFwnwRUL4bD"
+    exit 0
 else
     cd src
-    if [[ ! -e 'obj' ]]; then
-        sudo mkdir obj
-    elif [[ ! -d 'obj' ]]; then
-        output "Hey the developer did his job" 1>&2
-    fi
+    sudo mkdir -p obj
     cd leveldb
     sudo chmod +x build_detect_platform
     sudo make clean
